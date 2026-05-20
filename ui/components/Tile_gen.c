ff8e1b446d07eb0a1d0b052631ad4c62e32526f3
Manoj <58104995+BlackLopaz@users.noreply.github.com>
2026-05-20 18:56:47 +1200

    Fix tile layout

--- ui/components/Tile_gen.c
+++ ui/components/Tile_gen.c
@@ -34,6 +34,28 @@ lv_obj_t * Tile_create(lv_obj_t * parent, int32_t number, const char * text, int
 {
     LV_TRACE_OBJ_CREATE("begin");
 
+    static lv_style_t style_base;
+    static lv_style_t style_column;
+
+    static bool style_inited = false;
+
+    if (!style_inited) {
+        lv_style_init(&style_base);
+        lv_style_set_bg_opa(&style_base, (255 * 100 / 100));
+        lv_style_set_bg_color(&style_base, RB_RED);
+        lv_style_set_width(&style_base, LV_SIZE_CONTENT);
+        lv_style_set_height(&style_base, LV_SIZE_CONTENT);
+        lv_style_set_text_color(&style_base, lv_color_hex3(0x000));
+
+        lv_style_init(&style_column);
+        lv_style_set_flex_flow(&style_column, LV_FLEX_FLOW_COLUMN);
+        lv_style_set_flex_main_place(&style_column, LV_FLEX_ALIGN_CENTER);
+        lv_style_set_flex_cross_place(&style_column, LV_FLEX_ALIGN_CENTER);
+        lv_style_set_pad_row(&style_column, 4);
+
+        style_inited = true;
+    }
+
     lv_obj_t * lv_obj_0 = lv_obj_create(parent);
     lv_obj_set_name_static(lv_obj_0, "Tile_#");
 
@@ -42,11 +64,18 @@ lv_obj_t * Tile_create(lv_obj_t * parent, int32_t number, const char * text, int
     lv_obj_set_y(lv_obj_1, tile_pos_y);
     lv_obj_set_width(lv_obj_1, width);
     lv_obj_set_height(lv_obj_1, height);
+    lv_obj_set_style_layout(lv_obj_1, LV_LAYOUT_FLEX, 0);
+    lv_obj_add_style(lv_obj_1, &style_column, 0);
     lv_obj_t * lv_label_0 = lv_label_create(lv_obj_1);
     lv_label_set_text(lv_label_0, number);
+    lv_obj_set_align(lv_label_0, LV_ALIGN_CENTER);
+    lv_obj_set_style_text_align(lv_label_0, LV_TEXT_ALIGN_CENTER, 0);
     
     lv_obj_t * lv_label_1 = lv_label_create(lv_obj_1);
     lv_label_set_text(lv_label_1, text);
+    lv_obj_set_align(lv_label_1, LV_ALIGN_CENTER);
+    lv_obj_set_style_text_align(lv_label_1, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_add_style(lv_label_1, &style_base, 0);
 
     LV_TRACE_OBJ_CREATE("finished");
 
